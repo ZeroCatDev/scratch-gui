1c451ae60cf5c578bf9302741a180105bc76b350
Sunwuyuan <sun@wuyuan.dev>
2025-07-31 17:47:13 +0800

    添加新的素材管理系统

--- src/lib/storage.js
+++ src/lib/storage.js
@@ -55,7 +55,7 @@ class Storage extends ScratchStorage {
         this.assetHost = assetHost;
     }
     getAssetGetConfig (asset) {
-        return `${this.assetHost}/${asset.assetId}.${asset.dataFormat}`;
+        return `${this.assetHost}/${asset.assetId.slice(0, 2)}/${asset.assetId.slice(2, 4)}/${asset.assetId}.${asset.dataFormat}`;
     }
     getAssetCreateConfig (asset) {
         return {
